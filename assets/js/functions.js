f04153985ccc99f5bc12368e2fc23f3e93db94ba
bomba65 <bkalzhan@gmail.com>
2019-05-30 14:09:53 +0600

    Last changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
 
+  $(".phone-number").inputmask({"mask": "+7 (999) 999-9999"});
+
   $('.main-slider').slick({
     dots: true,
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     prevArrow: '<span class="icomoon icon-left-arrow"></span>',
     nextArrow: '<span class="icomoon icon-right-arrow"></span>'
@@ -19,6 +21,29 @@ $( document ).ready(function() {
     variableWidth: true
   });
 
+  $(document).ready(function() {
+  	$('.gallery-list-item').magnificPopup({
+  		delegate: 'a',
+  		type: 'image',
+  		tLoading: 'Loading image #%curr%...',
+  		mainClass: 'mfp-img-mobile',
+  		gallery: {
+  			enabled: true,
+  			navigateByImgClick: true,
+  			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  		},
+  		image: {
+  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+  			titleSrc: function(item) {
+  				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+  			}
+  		}
+  	});
+  });
+
+  $('.nav-link-first').click(function() {
+    $('.nav-link-first-menu').toggleClass('active');
+  })
 });
 
 // Change icon in map iframe
